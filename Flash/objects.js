105bb3fb841be366351aa4e62658c938fbbccdb3
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-06 11:17:40 -0400

    Create objects.js

---
+++ Flash/objects.js
@@ -0,0 +1,45 @@
+var objectlist = [];
+(function(ext) {
+    // When Scratch closes, do this
+    ext._shutdown = function() {
+        // Nothing here at the moment...
+    };
+ 
+    // Tell Scratch if the extension is ready to do stuff
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'}; // Status 1 = Red, Error // Status 1 = Yellow, Not Ready // Status 2 = Green, Ready
+    };
+ 
+    // Blocks to add to Scratch
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            [' ', 'set object %n property %s to %s', 'so', 0, "username", "AmazingMech2418"],
+            [' ', 'new object', 'newO'],
+            ['r', 'get object number %n property %s', 'go', 0, "username"],
+            ['r', 'number of objects','no']
+        ],
+menus: {
+txt: ['hi','bye','how are you?'],
+superMath: ['radian sine', 'radian cosine', 'radian tangent', 'asin', 'atan', 'acos', 'power'],
+geo: ['latitude','longitude']
+}
+    };
+ 
+    // What blocks do go here
+    ext.so = function (n,p,d) {
+    objectlist[n][p]=d;
+    }
+    ext.newO = function () {
+    objectlist.push(new Object());
+    }
+    ext.go = function (n,p) {
+    return objectlist[n][p];
+    }
+    ext.no = function() {
+    return objectlist.length;
+    }
+    // Name of Scratch Extension goes here
+    ScratchExtensions.register('Keys', descriptor, ext); 
+     // The name is 'Hello, World!''
+})({});
